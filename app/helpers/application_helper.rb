6abcae4920f0922d9d53eb457953b3959788a2d4
Thomé Lucas Oliveira <chacalgbi@hotmail.com>
2021-02-17 18:16:53 -0300

    Testes do Curso - Aula 10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def data_hora_br(date)
+        date.strftime("%H:%M %d/%m/%Y")
+    end
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
+
+    
 end
